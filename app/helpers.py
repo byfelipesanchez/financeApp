9a26ed7c9c551c7aeab623b92aefd8413fff3657
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-23 14:22:22 -0300

    still just experimenting with kivy. just added styling stuff at this point

---
+++ app/helpers.py
@@ -0,0 +1,8 @@
+username_helper = """
+MDTextField:
+    hint_text: "enter username"
+    helper_text: "or login"
+    helper_text_mode: "on_focus"  
+    pos_hint: {'center_x':0.5, 'center_y':0.5 }
+    size_hint_x: None 
+"""
--- app/main.py
+++ app/main.py
@@ -1,9 +1,40 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRoundFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivy.lang import Builder
+from helpers import username_helper
+from kivymd.uix.dialog import MDDialog
+
 
 class financeApp(MDApp):
+
     def build(self):
-        test = MDLabel(text='sup', halign='center')
-        return test
+        screen = Screen()
+
+        # username = MDTextField(text='Enter Username',
+        #                        pos_hint={'center_x':0.5, 'center_y':0.5 },
+        #                        size_hint_x=None, width=300)
+
+        button = MDRectangleFlatButton(text='show',
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.4},
+                                       on_release=self.show_data)
+        self.username = Builder.load_string(username_helper)
+        screen.add_widget(self.username)
+        screen.add_widget(button)
+        return screen
+
+    def show_data(self, obj):
+        close_btn = MDRoundFlatButton(text='Close', on_release=self.close_dialog)
+        self.dialog = MDDialog(title='User', text=self.username.text,
+                               size_hint=(0.7, 1),
+                               buttons=[close_btn])
+        self.dialog.open()
+        # print(self.username.text)
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
+
 
 financeApp().run()
