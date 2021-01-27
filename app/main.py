55a0fd00f8c14e648f28fd0a459d875612c79fad
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-27 13:29:24 -0300

    grid layout. next gonna try float layout

--- app/main.py
+++ app/main.py
@@ -1,50 +1,26 @@
 from kivymd.app import MDApp
-# from kivymd.uix.screen import Screen
-# from kivymd.uix.button import MDRectangleFlatButton
-# from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRoundFlatButton
-# from kivymd.uix.textfield import MDTextField
+from kivy.uix.widget import Widget
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRoundFlatButton
 from kivy.uix.label import Label
 # from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-# from helpers import username_helper
-# from kivymd.uix.dialog import MDDialog
-# from kivy.factory import Factory
-# from kivymd.uix.boxlayout import BoxLayout
-# # from helpers import KV
-# from kivymd.uix.button import MDFloatingActionButtonSpeedDial
-# from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
 
 
-# class financeApp(MDApp):
-#     def build(self):
-#         return Builder.load_string(KV)
+
+class Design(Widget):
+    test: ObjectProperty(None)
+
+    def btn(self):
+        print("test", self.test.text)
+        self.test.text = " "
 
 
-# class ContentNavigationDrawer(BoxLayout):
-#     pass
-
-class Design(GridLayout):
-    def __init__(self, **kwargs):
-        super(Design, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="name:"))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-
-
-class App(MDApp):
-    # data = {
-    #     'currency-usd-off': 'Expense',
-    #     'currency-usd': 'Saving'
-    #
-    # }
-    #
-    # self.add_widget
-
+class FinanceApp(MDApp):
     def build(self):
-        # self.theme_cls.theme_style = "Dark"  # "Light"
+        self.theme_cls.theme_style = "Dark"  # "Light"
         # return Builder.load_string(KV)
         return Design()
 
-        App().run()
+FinanceApp().run()
