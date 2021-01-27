895bb17916188ffeaeca30db89bf75e727ea13b0
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-27 14:09:09 -0300

    .

--- app/main.py
+++ app/main.py
@@ -2,25 +2,36 @@ from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRoundFlatButton
 from kivy.uix.label import Label
-# from kivy.lang import Builder
+from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+
+class MainWindow(Screen):
+    pass
+
+    def Widgets(Widgets):
+        pass
 
 
+class SecondWindow(Screen):
+    pass
 
-class Design(Widget):
-    test: ObjectProperty(None)
+class WindowManager(ScreenManager):
+    pass
 
-    def btn(self):
-        print("test", self.test.text)
-        self.test.text = " "
 
+kv = Builder.load_file("finance.kv")
 
 class FinanceApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"  # "Light"
         # return Builder.load_string(KV)
-        return Design()
+        return kv
 
 FinanceApp().run()
