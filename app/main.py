07645a8f8c363c15efa743223db2aefcffa37deb
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-28 20:01:07 -0300

    gotta fiz some errors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import self as self
 import sm as sm
+from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRoundFlatButton
 from kivy.lang import Builder
@@ -8,6 +9,8 @@ from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
+from database import DataBase
+
 
 class AccountWindow(Screen):
     name1 = ObjectProperty(None)
@@ -18,16 +21,32 @@ class AccountWindow(Screen):
         if self.name1.text != "" and self.email.text != "" and self.email.text.count(
                 "@") == 1 and self.email.text.count(".") > 0:
             if self.password.text != "":
-                #todo: add user to data base
                 self.reset()
 
-                sm.current = "login" #todo: into login window
+                sm.current = "login"
             else:
-                invalidInfo() #todo: crate invalidInfo method
+                invalidInfo()
 
     def login(self):
         self.reset()
         sm.current
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginButton(self):
+        if db.validation(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidInfo()
+
+    def createButton(self):
+        self.reset()
+        sm.current = "create"
 
     def reset(self):
         self.name1.text = ""
@@ -35,40 +54,48 @@ class AccountWindow(Screen):
         self.password.text = ""
 
 
-class loginWindow(Screen):
-    email = ObjectProperty(None)
-    password = ObjectProperty(None)
-
-    def loginButton(self):
-
-        #todo: validation on database
-
-class mainWindow(Screen):
-    accname: ObjectProperty(None)
+class MainWindow(Screen):
+    n: ObjectProperty(None)
     email: ObjectProperty(None)
     createdDate: ObjectProperty(None)
 
-    def logOut(self):
+    def log_out(self):
         sm.current("login")
 
-    #todo: def a 'subwindow' for when you enter displaying you info
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name " + name
+        self.email.text = "Email " + self.current
+        self.password.text = "Password " + password
+        self.created.text = "This Account Was Created On " + created
+
 
 class WindowManager(ScreenManager):
     pass
 
-def invalidInfo() #todo: as a popUp
+
+def invalidInfo():
+    popup = Popup(title="Invalid Information",
+                  content=Label(text="The Entered Username or Password is Incorrect"),
+                  size_hint=(None, None), size=(400, 400))
+    popup.open()
+
 
 kv = Builder.load_file("finance.kv")
 
 sm = WindowManager()
-#todo: database
+db = DataBase("users.txt")
+screens: [LoginWindow(name="login"), AccountWindow(name="create"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
 
-screens: [#todo]
+sm.current = "login"
+
 
 class FinanceApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"  # "Light"
-        # return Builder.load_string(KV)
-        return kv
+        return sm
+
 
 FinanceApp().run()
