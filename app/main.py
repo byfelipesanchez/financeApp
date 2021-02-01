556559e38bfb6b786aadcc3bd0bb42997eb94165
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-02-01 17:12:22 -0300

    took me soooooo long to fix this i spent 1 and a half hour with a guy in the kivy discord he helped me fix everything and now kivymd is implemented! thank god! the next thing i gotta do is actually change all the labels and textinputs and everything like that to md im so glad i fixed it!!!! so excited!

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
-from kivy.app import App
+from kivymd.app import MDApp
+# from kivymd.uix.label import MDLabel
 from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
+# from kivymd.uix.tab import MDTabsBase
 from database import DataBase
-from kivy.core.window import Window
-from kivy.properties import OptionProperty
+# from kivy.core.window import Window
+# from kivy.properties import OptionProperty
 from kivy.clock import Clock
 Clock.max_iteration = 50
+
+
 
 
 class AccountWindow(Screen):
@@ -24,7 +28,7 @@ class AccountWindow(Screen):
 
                 self.reset()
 
-                sm.current = "login"
+                self.manager.current = "login"
 
             else:
                 invalidInfo()
@@ -33,7 +37,7 @@ class AccountWindow(Screen):
 
     def login(self):
         self.reset()
-        sm.current = "login"
+        self.manager.current = "login"
 
     def reset(self):
         self.name1.text = ""
@@ -49,13 +53,13 @@ class LoginWindow(Screen):
         if db.validate(self.email.text, self.password.text):
             MainWindow.current = self.email.text
             self.reset()
-            sm.current = "main"
+            self.manager.current = "main"
         else:
             invalidLogin()
 
     def create_button(self):
         self.reset()
-        sm.current = "create"
+        self.manager.current = "create"
 
     def reset(self):
         self.email.text = ""
@@ -63,13 +67,13 @@ class LoginWindow(Screen):
 
 
 class MainWindow(Screen):
-    n: ObjectProperty(None)
-    email: ObjectProperty(None)
-    created: ObjectProperty(None)
+    n = ObjectProperty(None)
+    email = ObjectProperty(None)
+    created = ObjectProperty(None)
     current = ""
 
     def log_out(self):
-        sm.current = "login"
+        self.manager.current = "login"
 
     def on_enter(self, *args):
         password, name, created = db.get_user(self.current)
@@ -83,6 +87,7 @@ class WindowManager(ScreenManager):
     pass
 
 
+
 def invalidInfo():
     pop = Popup(title="Invalid Information", content=Label(text="Please Fill All The Required Inputs"),
                 size_hint=(None, None), size=(400, 400))
@@ -94,37 +99,23 @@ def invalidLogin():
     pop.open()
 
 
-kv = Builder.load_file("my.kv")
-
-sm = WindowManager()
 db = DataBase("users.txt")
 
-screens = [LoginWindow(name="login"), AccountWindow(name="create"), MainWindow(name="main")]
-for screen in screens:
-    sm.add_widget(screen)
 
-sm.current = "login"
+class MainApp(MDApp):
 
-
-class MyFinanceApp(App):
-    media = OptionProperty('M', options=('XS', 'S', 'M', 'L', 'XL'))
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.theme_cls.theme_style = "Dark"
 
     def build(self):
-        Window.clearcolor = (0, 0.50196078431, 0.50196078431, 0.00392156862)
-        Window.bind(size=self.update_media)
+        screens = [LoginWindow(name="login"), AccountWindow(name="create"), MainWindow(name="main")]
+        sm = ScreenManager()
+        for screen in screens:
+            sm.add_widget(screen)
+            sm.current = "login"
         return sm
 
-    def update_media(self, win, size):
-        width, height = size
-        self.media = (
-            'XS' if width < 250 else
-            'S' if width < 500 else
-            'M' if width < 1000 else
-            'L' if width < 1200 else
-            'XL'
-        )
-
-
 if __name__ == "__main__":
-    MyFinanceApp().run()
+    MainApp().run()
 
